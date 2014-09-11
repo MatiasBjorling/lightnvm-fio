86601eee0d545a91f4c92e12f10031013d416cdd
Matias Bjørling <m@bjorling.me>
2014-09-11 11:54:36 +0200

    vsl: DMA align xfer_buf allocation
    Allow us to take advantage of directly use the user-space allocated
    buffer

--- engines/kv.c
+++ engines/kv.c
@@ -114,7 +114,7 @@ static struct ioengine_ops ioengine = {
 	.open_file	= generic_open_file,
 	.close_file	= generic_close_file,
 	.get_file_size	= generic_get_file_size,
-	.flags		= FIO_SYNCIO,
+	.flags		= FIO_SYNCIO | FIO_RAWIO | FIO_MEMALIGN,
 };
 
 static void fio_init fio_kv_register(void)
